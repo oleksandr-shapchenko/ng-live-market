13890368e5071d10420d6b27c50959d1d94b4b30
Oleksandr Shapchenko <aleksandrshapchenko@gmail.com>
2024-10-15 19:41:03 +0300

    Add environment to .gitignore

--- src/environments/environment.dev.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  fintachartsBaseUrl: 'https://platform.fintacharts.com',
-  fintachartsBaseWsUrl: 'wss://platform.fintacharts.com',
-  /** @desc Add your username here to authenticate */
-  username: '',
-  /** @desc Add your password here to authenticate */
-  password: '',
-};
