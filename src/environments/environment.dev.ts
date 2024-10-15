af675e03b75d15a375385fe6f7b675257c8219a2
Oleksandr Shapchenko <aleksandrshapchenko@gmail.com>
2024-10-15 19:35:52 +0300

    Add environment const

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  fintachartsBaseUrl: 'https://platform.fintacharts.com',
+  fintachartsBaseWsUrl: 'wss://platform.fintacharts.com',
+  /** @desc Add your username here to authenticate */
+  username: '',
+  /** @desc Add your password here to authenticate */
+  password: '',
+};
